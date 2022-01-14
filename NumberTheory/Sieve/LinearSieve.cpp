60efbadb0402c4d0397c45c1f6d4db20e32c9243
navjotdadwal <59385365+navjotdadwal@users.noreply.github.com>
2022-01-14 08:04:15 +0530

    Update LinearSieve.cpp

--- NumberTheory/Sieve/LinearSieve.cpp
+++ NumberTheory/Sieve/LinearSieve.cpp
@@ -1,13 +1,23 @@
-const int N = 10000000;
-vector<int> lp(N+1);
-vector<int> pr;
+#include<bits/stdc++.h>
+using namespace std;
 
-for (int i=2; i <= N; ++i) {
-    if (lp[i] == 0) {
-        lp[i] = i;
-        pr.push_back(i);
+const int N = 1e5 + 9;
+
+int spf[N];
+vector<int> primes;
+void sieve() {
+  for(int i = 2; i < N; i++) {
+    if (spf[i] == 0) spf[i] = i, primes.push_back(i);
+    int sz = primes.size();
+    for (int j = 0; j < sz && i * primes[j] < N && primes[j] <= spf[i]; j++) {
+      spf[i * primes[j]] = primes[j];
     }
-    for (int j=0; j < (int)pr.size() && pr[j] <= lp[i] && i*pr[j] <= N; ++j) {
-        lp[i * pr[j]] = pr[j];
-    }
+  }
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  
+  return 0;
 }
